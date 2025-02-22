3079b0386fe2133de3f32bb9a547cd21d8a82dac
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-02-22 12:25:28 +0200

    Project initial commit

---
+++ src/main/java/com/lehdonjoki/listoo/ListooApplication.java
@@ -0,0 +1,13 @@
+package com.lehdonjoki.listoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListooApplication.class, args);
+	}
+
+}
