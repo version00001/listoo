89e1c0132c5bfc62389aed8d4b99ca4f5dca70ae
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-02-24 23:14:01 +0200

    Add repository classes for Item, ShoppingList, and User

---
+++ src/main/java/com/lehdonjoki/listoo/repository/ItemRepository.java
@@ -0,0 +1,5 @@
+package com.lehdonjoki.listoo.repository;
+
+public class ItemRepository {
+    
+}
---
+++ src/main/java/com/lehdonjoki/listoo/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.lehdonjoki.listoo.repository;
+
+public class UserRepository {
+    
+}
