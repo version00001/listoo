b9d5fb10f19e644208ca7d072e64c60c8de3b510
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-02-24 23:25:53 +0200

    Remove roles field from User model

--- src/main/java/com/lehdonjoki/listoo/model/User.java
+++ src/main/java/com/lehdonjoki/listoo/model/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.Set;
 
 @Entity
 @Data
@@ -25,9 +24,4 @@ public class User {
 
     @Column(nullable = false)
     private String name;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "role")
-    private Set<String> roles;
 }
