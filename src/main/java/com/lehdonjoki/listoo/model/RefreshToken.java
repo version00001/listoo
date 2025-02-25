0a0f00b53e0f4ee53955aa84e92758543dd6be2c
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-02-25 22:31:57 +0200

    Add RefreshToken model to support token management

---
+++ src/main/java/com/lehdonjoki/listoo/model/RefreshToken.java
@@ -0,0 +1,29 @@
+package com.lehdonjoki.listoo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Date expiryDate;
+}
