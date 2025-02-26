031884902abe72d43a707d8b9b207069857b5975
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-02-27 00:09:29 +0200

    Enable method security in SecurityConfig to support @PreAuthorize and @PostAuthorize annotations

--- src/main/java/com/lehdonjoki/listoo/security/SecurityConfig.java
+++ src/main/java/com/lehdonjoki/listoo/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ import com.lehdonjoki.listoo.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true) // Enables @PreAuthorize and @PostAuthorize
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
