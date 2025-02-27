71b2a1dc73d0a9b747293d155d6d5d82b63a2219
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-02-27 20:13:04 +0200

    Add ShoppingListRepository interface with JPA support and custom query methods

--- src/main/java/com/lehdonjoki/listoo/repository/ShoppingListRepository.java
+++ src/main/java/com/lehdonjoki/listoo/repository/ShoppingListRepository.java
@@ -1,5 +1,13 @@
 package com.lehdonjoki.listoo.repository;
 
-public class ShoppingListRepository {
-    
+import com.lehdonjoki.listoo.model.ShoppingList;
+import com.lehdonjoki.listoo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
+    List<ShoppingList> findByUser(User user);
+    Optional<ShoppingList> findByIdAndUserEmail(Long id, String email);
 }
