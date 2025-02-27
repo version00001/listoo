ec2edc5dd0f4910a858e43b509c594f4e18b2684
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-02-27 20:12:59 +0200

    Add ShoppingList entity with JPA annotations and relationships

--- src/main/java/com/lehdonjoki/listoo/model/ShoppingList.java
+++ src/main/java/com/lehdonjoki/listoo/model/ShoppingList.java
@@ -1,5 +1,32 @@
 package com.lehdonjoki.listoo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShoppingList {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private boolean archived = false;
+
+    public ShoppingList(String name, User user) {
+        this.name = name;
+        this.user = user;
+        this.archived = false;
+    }
 }
