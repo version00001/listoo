dd8a61a59ec02aa3d5747d7fc42bcbaa6b21af96
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-03-05 23:14:25 +0200

    Add TestingDatabase annotation for integration testing with Flyway and embedded PostgreSQL

---
+++ src/test/java/com/lehdonjoki/listoo/testutils/TestingDatabase.java
@@ -0,0 +1,19 @@
+package com.lehdonjoki.listoo.testutils;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.flywaydb.test.annotation.FlywayTest;
+import org.springframework.test.context.TestPropertySource;
+
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+
+@FlywayTest
+@TestPropertySource(properties = {"zonky.test.database.postgres.docker.image=postgres:16-alpine"})
+@AutoConfigureEmbeddedDatabase(
+    type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES,
+    provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.DOCKER,
+    refresh = AutoConfigureEmbeddedDatabase.RefreshMode.BEFORE_EACH_TEST_METHOD
+)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TestingDatabase {}
