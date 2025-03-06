6c28651a05f3331a32e2df3e264fe34123dad859
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-03-06 20:26:22 +0200

    Add TestingDatabase annotation to ListooApplicationTests for integration testing

--- src/test/java/com/lehdonjoki/listoo/ListooApplicationTests.java
+++ src/test/java/com/lehdonjoki/listoo/ListooApplicationTests.java
@@ -3,7 +3,10 @@ package com.lehdonjoki.listoo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.lehdonjoki.listoo.testutils.TestingDatabase;
+
 @SpringBootTest
+@TestingDatabase
 class ListooApplicationTests {
 
   @Test
