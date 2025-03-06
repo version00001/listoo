b32808d726e305d536c8c67b43228fe4c92dc921
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-03-06 23:47:50 +0200

    Refactor ListooApplicationTests to streamline imports

--- src/test/java/com/lehdonjoki/listoo/ListooApplicationTests.java
+++ src/test/java/com/lehdonjoki/listoo/ListooApplicationTests.java
@@ -1,9 +1,8 @@
 package com.lehdonjoki.listoo;
 
+import com.lehdonjoki.listoo.testutils.TestingDatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.lehdonjoki.listoo.testutils.TestingDatabase;
 
 @SpringBootTest
 @TestingDatabase
