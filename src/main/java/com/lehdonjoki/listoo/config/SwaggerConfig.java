13710add02c22169a37c824a7b9e48d640a98e71
Eerik Lehtomäki <eerik.lehtomaki@gmail.com>
2025-03-07 22:44:11 +0200

    Add security scheme for Bearer authentication in Swagger configuration

--- src/main/java/com/lehdonjoki/listoo/config/SwaggerConfig.java
+++ src/main/java/com/lehdonjoki/listoo/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.lehdonjoki.listoo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,16 @@ public class SwaggerConfig {
             new Info()
                 .title("Shopping List API")
                 .version("1.0")
-                .description("API documentation for the Shopping List App Backend"));
+                .description("API documentation for the Shopping List App Backend"))
+        .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+        .components(
+            new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes(
+                    "BearerAuth",
+                    new SecurityScheme()
+                        .name("BearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
   }
 }
